e738e6c2c29ef02b114d41c1f7626264a1668526
Timur Kutuev <thedogmrprogrammertimkut86yeah@gmail.com>
2026-06-11 20:35:43 +0700

    .

--- static/js/check.js
+++ static/js/check.js
@@ -1,10 +1,11 @@
-const _at = document.getElementById("ad-test");
+let _at = document.getElementById("ad");
+let _ats = _at.style;
 
 check();
 
 function check(){
-    _at.style.display = 'flex';
-    if(_at.style.display == 'none'){
+    _ats.display = 'flex';
+    if(_ats.display == 'none'){
         console.log("The SET can't work with AdBlock. Turn off that for SET can work.");
     }
 }
