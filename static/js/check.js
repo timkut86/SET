37c8200c94f327d94af24cb7c44e5013c84e8e81
Timur Kutuev <thedogmrprogrammertimkut86yeah@gmail.com>
2026-06-11 22:35:10 +0700

    Update check.js

--- static/js/check.js
+++ static/js/check.js
@@ -1,11 +1,10 @@
 let _at = document.getElementById("ads");
-let _ats = _at.style;
+let _ats = _at.offsetHeight;
 
 check();
 
 function check(){
-    _ats.height = '272px';
-    if(_ats.height == '0'){
+    if(_ats == '0'){
         console.log("The SET cannot work with AdBlock. Turn off that for SET can work.");
     } else console.log("The SET loaded succesfully.")
 }
